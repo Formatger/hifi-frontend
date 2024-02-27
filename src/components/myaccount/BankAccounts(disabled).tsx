dfbddb0df6c1a9be54642d2aa640d76bc70cee05
Storm <stormeverrise@gmail.com>
2024-02-27 17:15:43 +0100

    fixes

--- src/components/myaccount/BankAccounts(disabled).tsx
+++ src/components/myaccount/BankAccounts(disabled).tsx
@@ -1,48 +1,48 @@
-// import React, { useState } from "react";
-// import Entry from "@/components/myaccount/Entry";
-// import AddAccountModal from "./AddAccountModal";
+import React, { useState } from "react";
+import Entry from "@/components/myaccount/Entry";
+import AddAccountModal from "./AddAccountModal";
 
-// interface BankAccountsProps {
-//   entries: string[];
-// }
+interface BankAccountsProps {
+  entries: string[];
+}
 
-// const BankAccounts: React.FC<BankAccountsProps> = ({ entries }) => {
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-//   const openModal = () => {
-//     setIsModalOpen(true);
-//   };
+const BankAccounts: React.FC<BankAccountsProps> = ({ entries }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
 
-//   const closeModal = () => {
-//     setIsModalOpen(false);
-//   };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
-//   return (
-//     <div className="w-full xl:w-[452px] p-5 bg-white rounded-lg shadow border border-gray-200 flex-col justify-start items-start gap-5 inline-flex">
-//       <section className="w-full flex flex-col lg:flex-row items-start lg:items-center justify-between">
-//         <h2 className="text-[#111012] text-lg lg:text-2xl font-semibold poppins-remove leading-loose">
-//           Bank Accounts
-//         </h2>
-//         <button
-//           className="bg-indigo-900 w-[130px] h-7 lg:h-8 text-sm lg:text-remove rounded-md flex text-stone-50 items-center justify-center poppins-remove"
-//           onClick={openModal}
-//         >
-//           Add Account
-//         </button>
-//       </section>
-//       <section className="w-full flex-col gap-5">
-//         {entries.map((entry) => (
-//           <Entry title={entry} key={entry} />
-//         ))}
-//       </section>
-//       {isModalOpen && (
-//         <AddAccountModal
-//           isOpen={isModalOpen}
-//           closeModal={closeModal}
-//           isEdit={false}
-//         />
-//       )}
-//     </div>
-//   );
-// };
+  return (
+    <div className="w-full xl:w-[452px] p-5 bg-white rounded-lg shadow border border-gray-200 flex-col justify-start items-start gap-5 inline-flex">
+      <section className="w-full flex flex-col lg:flex-row items-start lg:items-center justify-between">
+        <h2 className="text-[#111012] text-lg lg:text-2xl font-semibold poppins-remove leading-loose">
+          Bank Accounts
+        </h2>
+        <button
+          className="bg-indigo-900 w-[130px] h-7 lg:h-8 text-sm lg:text-remove rounded-md flex text-stone-50 items-center justify-center poppins-remove"
+          onClick={openModal}
+        >
+          Add Account
+        </button>
+      </section>
+      <section className="w-full flex-col gap-5">
+        {entries.map((entry) => (
+          <Entry title={entry} key={entry} />
+        ))}
+      </section>
+      {isModalOpen && (
+        <AddAccountModal
+          isOpen={isModalOpen}
+          closeModal={closeModal}
+          isEdit={false}
+        />
+      )}
+    </div>
+  );
+};
 
-// export default BankAccounts;
+export default BankAccounts;
